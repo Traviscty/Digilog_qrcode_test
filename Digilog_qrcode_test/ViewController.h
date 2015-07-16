8eddc6378b78b3730bbc3734764430541f2dad01
C-ty <cty@ctyde-macbook.local>
2015-07-16 13:20:22 +0800

    Initial Commit

---
+++ Digilog_qrcode_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Digilog_qrcode_test
+//
+//  Created by C-ty on 2015/7/16.
+//  Copyright (c) 2015年 Cty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Digilog_qrcode_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Digilog_qrcode_test
+//
+//  Created by C-ty on 2015/7/16.
+//  Copyright (c) 2015年 Cty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
